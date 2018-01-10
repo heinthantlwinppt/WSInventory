5949b0c3a681f9d9a34250bb88eebfe8f5c4a170
ZinWaiYan-ppt <zinwaiyan.ppt@gmail.com>
2018-01-10 15:07:38 +0630

    add reloadfragment

--- app/src/main/java/com/ppt/wsinventory/wsdb/DbAccess.java
+++ app/src/main/java/com/ppt/wsinventory/wsdb/DbAccess.java
@@ -19,7 +19,9 @@ import com.ppt.wsinventory.model.Solution;
 import com.ppt.wsinventory.util.Utility;
 
 import java.io.File;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -211,6 +213,55 @@ public class DbAccess {
         return dashboarditems;
 
 
+    }
+
+    public AdministrationSettings getAdministrationSettings() {
+//        AdministrationSettings administrationSettings = new AdministrationSettings();
+        AdministrationSettings administrationSetting = new AdministrationSettings();
+            Cursor cursor = readData(AdministrationSettings.TABLE_ADMINISTRATION_SETTINGS
+                    , AdministrationSettings.COLUMN_ALL
+                    , null, null, null, null, null
+//                , new String[]{"1"}, null, null, null
+            );
+
+            while (cursor.moveToNext()) {
+                administrationSetting = new AdministrationSettings();
+                administrationSetting.setId(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_ID)));
+                administrationSetting.setH1(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_H1)));
+                administrationSetting.setH2(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_H2)));
+                administrationSetting.setH3(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_H3)));
+                administrationSetting.setH4(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_H4)));
+                administrationSetting.setT1(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_T1)));
+                administrationSetting.setT2(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_T2)));
+                administrationSetting.setT3(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_T3)));
+                administrationSetting.setT4(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_T4)));
+                administrationSetting.setDateformat(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_DATEFORMAT)));
+                administrationSetting.setTimeformat(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_TIMEFORMAT)));
+                administrationSetting.setDatetimeformat(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_DATETIMEFORMAT)));
+                try {
+                    administrationSetting.setSystem_date(Utility.dateFormat.parse(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_SYSTTEM_DATE))));
+                } catch (ParseException e) {
+                    administrationSetting.setSystem_date(new Date(System.currentTimeMillis()));
+                }
+                administrationSetting.setActive(Boolean.parseBoolean(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_ACTIVE))));
+                administrationSetting.setLocation_id(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_LOCATION_ID)));
+                administrationSetting.setSolution_id(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_SOLUTION_ID)));
+                administrationSetting.setStaff_id(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_STAFF_ID)));
+                administrationSetting.setWeighscales_id(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_WEIGHSCALES_ID)));
+                administrationSetting.setDashboarditemlwith(Integer.parseInt(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_DASHBOARDITEMLWITH))));
+                administrationSetting.setDashboarditempwith(Integer.parseInt(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_DASHBOARDITEMPWITH))));
+                administrationSetting.setDashboardicon(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_DASHBOARDICON)));
+                administrationSetting.setDevicetype_id(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_DEVICETYPE_ID)));
+                administrationSetting.setDevice_id(cursor.getString(cursor.getColumnIndex(administrationSetting.COLUMN_DEVICE_ID)));
+
+//            administrationSettings.add(administrationSetting);
+            }
+
+            if (cursor != null && !cursor.isClosed()) {
+                cursor.close();
+            }
+
+        return administrationSetting;
     }
 
     public Item insertItems(Item item){
