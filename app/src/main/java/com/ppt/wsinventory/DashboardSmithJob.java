a650efd7edf9bc6e3ec554c7c47c34d01a448902
ZinWaiYan-ppt <zinwaiyan.ppt@gmail.com>
2018-01-15 11:53:43 +0630

    add smithjoborder

---
+++ app/src/main/java/com/ppt/wsinventory/DashboardSmithJob.java
@@ -0,0 +1,70 @@
+package com.ppt.wsinventory;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.ppt.wsinventory.model.AdministrationWsdashboard;
+import com.ppt.wsinventory.wsdb.DbAccess;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardSmithJob extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    DbAccess dbAccess;
+    List<AdministrationWsdashboard> smithJobOrderList = new ArrayList<>();
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+                case R.id.navigation_dashboard1:
+                    mTextMessage.setText("List");
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_smith_job);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.clearAnimation();
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+//        dbAccess = new DbAccess(this);
+//        dbAccess.open();
+////            minflater = inflater;
+//        smithJobOrderList = dbAccess.getAllDashboardItems();
+//        SmithJobOrderAdapter adapter = new SmithJobOrderAdapter( this, smithJobOrderList);
+        ListView listView = findViewById(R.id.lv_smithorder);
+//        listView.setAdapter(adapter);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/ppt/wsinventory/SmithJobOrderAdapter.java
@@ -0,0 +1,57 @@
+package com.ppt.wsinventory;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.ppt.wsinventory.model.AdministrationStaff;
+import com.ppt.wsinventory.model.AdministrationWsdashboard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+/**
+ * Created by User on 15/01/2018.
+ */
+
+public class SmithJobOrderAdapter extends ArrayAdapter {
+    ArrayList<AdministrationWsdashboard> mDataSet;
+    LayoutInflater mInflater;
+    @BindView(R.id.OrderName)
+    TextView txt_OrderName;
+    @BindView(R.id.OrderItem)
+    TextView txt_OrderItem;
+    @BindView(R.id.textView)
+    TextView txt_textView;
+    @BindView(R.id.textView1)
+    TextView txt_textView1;
+
+    public SmithJobOrderAdapter(@NonNull Context context, @NonNull List<AdministrationWsdashboard> objects) {
+        super(context, R.layout.smithjoborder_items, objects);
+        mInflater = LayoutInflater.from(context);
+        mDataSet = (ArrayList<AdministrationWsdashboard>) objects;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.smithjoborder_items, parent, false);
+        }
+
+        AdministrationWsdashboard administrationWsdashboard = mDataSet.get(position);
+        txt_OrderName.setText(administrationWsdashboard.getTitle());
+        txt_OrderItem.setText(administrationWsdashboard.getActionname());
+
+
+        return convertView;
+    }
+}
